f983d5cae287ba0ca7ce0e41ca4f71952d2ff46b
logisticsc <68974699+logisticsc@users.noreply.github.com>
2022-05-28 16:59:12 -0700

    Add files via upload

---
+++ tel.php
@@ -0,0 +1,9 @@
+$apiToken = "5557966069:AAGvhAwYnmlkL0_zw00mU3N0Dlin21hCrVU";
+
+$data = [
+    'chat_id' => '@scarfazebase',
+    'text' => 'Hello world!'
+];
+
+$response = file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?" . http_build_query($data) );
+// Do what you want with result
