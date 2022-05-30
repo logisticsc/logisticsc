46025fcf6729a52e9128bc2f36964bb89ab44e78
logisticsc <68974699+logisticsc@users.noreply.github.com>
2022-05-30 04:43:16 -0700

    Delete tel.php

--- tel.php
+++
@@ -1,9 +0,0 @@
-$apiToken = "5582926142:AAG97eIZhmzGVvEvKoCtzU7RMCAJUoppHcQ";
-
-$data = [
-    'chat_id' => '@dfgasq',
-    'text' => 'Hello world!'
-];
-
-$response = file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?" . http_build_query($data) );
-// Do what you want with result
